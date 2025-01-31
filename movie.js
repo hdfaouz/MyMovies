a6aa117742f6d74c12d8fedde24a95e3387c44f3
hdfaouz <hdfaouzia2@gmail.com>
2025-01-31 20:25:20 +0100

    modification

--- movie.js
+++ movie.js
@@ -26,7 +26,7 @@ function Ajout() {
         return;
     }
 
-    
+    // Création de l'objet film
     const objet = {
         titleValue,
         genreValue,
@@ -42,6 +42,8 @@ function Ajout() {
     console.log(Movies);
 
     afficherFilms(); // Mettre à jour l'affichage
+
+    window.location.href ="movie.html";
  
 }
 
@@ -62,6 +64,77 @@ function showPreview(event) {
     }
 }
 
+//Clear input 
+
+
+
+function clearData(){
+
+    Title.value = '';
+    genre.vlue = '';
+    Real.value = '';
+    Statut.value = '';
+    anSort.value = '';
+
+}
+
+// Fonction pour afficher les films sur la page d'accueil
+function afficherFilms() {
+    const filmsContainer = document.querySelector("#film-list");
+    
+    if (!filmsContainer) {
+        console.error("Erreur : filmsContainer est introuvable !");
+        return;
+    }
+    filmsContainer.innerHTML =""; // Vider le conteneur avant d'ajouter les nouveaux films
+    const Movies = JSON.parse(localStorage.getItem("Movies")) || [];
+
+    Movies.forEach((film) => {
+        const filmElement = document.createElement("div");
+        filmElement.classList.add("film");
+
+        filmElement.innerHTML = `
+        
+        <div class="test"  style: display=flex; >
+            <img src="${film.image}" alt="${film.titleValue}" Style=" width: 200px;height: 200px; margin-top:200px; display: flex;">
+            <h3>${film.titleValue}</h3>
+           
+            <button onclick="location.href=''"class="Modifier">Modifier</button>
+            <button onclick="location.href=''" class="Supprimer">Supprimer</button>
+            <button  class="detail" onclick="location.href='detail.html'">Detail</button>
+            </div>
+            
+        `;
+
+        filmsContainer.appendChild(filmElement);
+    });
+}
+
+// Fonction pour supprimer un film
+function supprimerFilm(index) {
+    let Movies = JSON.parse(localStorage.getItem("Movies")) || [];
+    Movies.splice(index, 1);
+    localStorage.setItem("Movies", JSON.stringify(Movies));
+    afficherFilms();
+}
+
+// Fonction pour modifier un film (redirection vers une page de modification)
+function modifierFilm(index) {
+    localStorage.setItem("filmModifier", index);
+    window.location.href = "modifier.html";
+}
+
+// Ajouter un événement au bouton pour ajouter un film
+const button = document.getElementById("btn_ajouter");
+button?.addEventListener('click', ()=>{
+    Ajout();
+    clearData();
+
+});
+    
+
+// Charger les films au démarrage de la page
+// document.addEventListener("DOMContentLoaded", afficherFilms);
 
 
 
